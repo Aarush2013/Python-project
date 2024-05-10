5ed58ab03a8ee3d6c26b5c5424a220eff5e4c76b
Aarush2013 <aarush.bodapati@gmail.com>
2024-05-10 17:39:48 +0530

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,4 @@
+fahrenheit=int(input("enter the value in fahrenheit: "))
+celsius=(fahrenheit-32)/1.8
+celsius=round(celsius,2)
+print(fahrenheit,"degree fahrenheit is equal to" ,celsius, "degree celsius")
