513cb0ab281a558033f8129d6309bcd605ab9177
Aarush2013 <aarush.bodapati@gmail.com>
2024-05-23 18:10:53 +0530

    Add files via upload

---
+++ Project 2.py
@@ -0,0 +1,9 @@
+trafficlight=input("enter a colour from trafficlight red,yellow,green: ")
+if trafficlight=="red":
+    print("Stop")
+elif trafficlight=="yellow":
+    print("Wait")
+elif trafficlight=="green":
+    print("Go")
+else:
+    print("choose from the options above")
